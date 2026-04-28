0d1a7afd0d6443a57fe686ff3116adbe4761cda3
Harshit Porwal <porwal1996harshit@gmail.com>
2026-04-28 13:44:05 +0530

    Update script.py

--- script.py
+++ script.py
@@ -8,9 +8,9 @@ import os
 # =========================
 # ENV VARIABLES
 # =========================
-RC_CLIENT_ID = os.environ['RC_CLIENT_ID']
-RC_CLIENT_SECRET = os.environ['RC_CLIENT_SECRET']
-RC_JWT = os.environ['RC_JWT']
+RC_CLIENT_ID = "Woq03G5Xe3AcnTmPOfY8Zx"
+RC_CLIENT_SECRET = "cfUpIy6TDMDf0rKn6d4efrfCdta60HXjcbfwdTxbVWFV"
+RC_JWT = "eyJraWQiOiI4NzYyZjU5OGQwNTk0NGRiODZiZjVjYTk3ODA0NzYwOCIsInR5cCI6IkpXVCIsImFsZyI6IlJTMjU2In0.eyJhdWQiOiJodHRwczovL3BsYXRmb3JtLnJpbmdjZW50cmFsLmNvbS9yZXN0YXBpL29hdXRoL3Rva2VuIiwic3ViIjoiNzkyNzcyMDM1IiwiaXNzIjoiaHR0cHM6Ly9wbGF0Zm9ybS5yaW5nY2VudHJhbC5jb20iLCJleHAiOjM5MjQ4NDE3OTcsImlhdCI6MTc3NzM1ODE1MCwianRpIjoiV0pfS2ZMNWlSNGlrbWZ5ZWhHWFZEUSJ9.DVxxxGNC8FInB_2ls3TxuJU9wOk8j_BXDq7n59Y5naVKV4qR-CyrlY98uU0x1jaFLSL87jYg00ir1G6AMvvC9tn8qwfvVS9FdUQSDwtJ4BBjtjWw3UbiwmjC07dRfZ93wvO40PvvjuuJxbkb6OZSE4ewHU6g0TxpCXJvzcf-ajmqnfyG8JS2wiCciLYMMHmYdtcj_qRPDlgnnIk7C6fiDK3Z9Cwptu56RhFUBoWh8TC0ROCTBhMXwd-f3PF70pDlbBOhgDKj2XjOZzVJF1c1gYOfCMlj8GUaGT_phaZVwYu6ryGzEhcwLtt_l62nzKoBaQJlDKmmfyknJbduHe8XYg"
 
 FILE_PATH = "ringcentral_call_logs.csv"
 
